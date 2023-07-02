bc9103668bf4f378236ea37392cdca71559e8756
Garv Shah <gshah.6110@gmail.com>
2023-07-02 15:39:01 +1000

    added the ability to upload images

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -6,6 +6,13 @@ Created: Sat Jun 18 18:29:00 2022
 Doc Link: https://github.com/cgs-math/app#theme-data
  */
 
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:mime/mime.dart';
+import 'package:path/path.dart';
+import 'package:uuid/uuid.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/material.dart';
 import 'package:maths_club/utils/plugins/actions/block_action_list.dart';
@@ -13,6 +20,8 @@ import 'package:maths_club/utils/plugins/actions/option_action.dart';
 import 'package:maths_club/utils/plugins/math_equation/math_equation_block_component.dart';
 import 'package:flowy_infra/theme_extension.dart';
 import 'package:flowy_infra/colorscheme/colorscheme.dart';
+import 'package:http/http.dart' as http;
+import 'package:universal_io/io.dart';
 
 TextStyle _getFontStyle({
   String? fontFamily,
@@ -360,14 +369,189 @@ Map<String, BlockComponentBuilder> getCustomBlockComponentBuilderMap(BuildContex
   return builders;
 }
 
-CharacterShortcutEvent slashMenu(BuildContext context) {
-  return customSlashCommand([mathEquationItem],
+CharacterShortcutEvent slashMenu(BuildContext parentContext) {
+  return customSlashCommand([
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.text,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'text',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['text'],
+      handler: (editorState, _, __) {
+        insertNodeAfterSelection(editorState, paragraphNode());
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.heading1,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'h1',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['heading 1, h1'],
+      handler: (editorState, _, __) {
+        insertHeadingAfterSelection(editorState, 1);
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.heading2,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'h2',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['heading 2, h2'],
+      handler: (editorState, _, __) {
+        insertHeadingAfterSelection(editorState, 2);
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.heading3,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'h3',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['heading 3, h3'],
+      handler: (editorState, _, __) {
+        insertHeadingAfterSelection(editorState, 3);
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.image,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'image',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['image'],
+      handler: (editorState, menuService, context) {
+        final container = Overlay.of(context);
+        showImageMenu(container, editorState, menuService,
+            onInsertImage: (imageUrl) async {
+              http.Response res;
+              try {
+                res = await http.get(Uri.parse(imageUrl));
+                // if status code of remote image is 200, then insert it!
+                if (res.statusCode == 200) editorState.insertImageNode(imageUrl);
+              } catch (e) {
+                // image is most likely a local file
+                print(imageUrl);
+                File file = File(imageUrl);
+                String? remoteUrl = await uploadFile(file.readAsBytesSync(), basename(imageUrl), extension(imageUrl), parentContext);
+                if (remoteUrl != null) editorState.insertImageNode(remoteUrl);
+              }
+            }
+        );
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.bulletedList,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'bulleted_list',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['bulleted list', 'list', 'unordered list'],
+      handler: (editorState, _, __) {
+        insertBulletedListAfterSelection(editorState);
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.numberedList,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'number',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['numbered list', 'list', 'ordered list'],
+      handler: (editorState, _, __) {
+        insertNumberedListAfterSelection(editorState);
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.checkbox,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'checkbox',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['todo list', 'list', 'checkbox list'],
+      handler: (editorState, _, __) {
+        insertCheckboxAfterSelection(editorState);
+      },
+    ),
+    SelectionMenuItem(
+      name: AppFlowyEditorLocalizations.current.quote,
+      icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+        name: 'quote',
+        isSelected: isSelected,
+        style: style,
+      ),
+      keywords: ['quote', 'refer'],
+      handler: (editorState, _, __) {
+        insertQuoteAfterSelection(editorState);
+      },
+    ),
+    dividerMenuItem,
+    mathEquationItem
+  ],
       style: SelectionMenuStyle(
-          selectionMenuBackgroundColor: Theme.of(context).cardColor,
-          selectionMenuItemTextColor: Theme.of(context).primaryColorLight,
-          selectionMenuItemIconColor: Theme.of(context).primaryColorLight,
-          selectionMenuItemSelectedTextColor: Theme.of(context).colorScheme.primary,
-          selectionMenuItemSelectedIconColor: Theme.of(context).colorScheme.primary,
-          selectionMenuItemSelectedColor: Theme.of(context).colorScheme.primary.withAlpha(45))
+          selectionMenuBackgroundColor: Theme.of(parentContext).cardColor,
+          selectionMenuItemTextColor: Theme.of(parentContext).primaryColorLight,
+          selectionMenuItemIconColor: Theme.of(parentContext).primaryColorLight,
+          selectionMenuItemSelectedTextColor: Theme.of(parentContext).colorScheme.primary,
+          selectionMenuItemSelectedIconColor: Theme.of(parentContext).colorScheme.primary,
+          selectionMenuItemSelectedColor: Theme.of(parentContext).colorScheme.primary.withAlpha(45))
   );
+}
+
+// Uploads files to Firebase and returns the download url
+Future<String?> uploadFile(Uint8List file, String name, String fileExtension, BuildContext context) async {
+  // Check if the file is too large
+  int size = file.lengthInBytes;
+  // Checks if the file is above 10mb
+  bool tooBig = size > 10000000;
+
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        tooBig ? "Files cannot be larger than 10mb!" : "Uploading file!",
+        style: TextStyle(
+            color:
+            Theme.of(context).primaryColorLight),
+      ),
+      backgroundColor:
+      Theme.of(context).scaffoldBackgroundColor,
+    ),
+  );
+
+  if (!tooBig) {
+    // Storage instance.
+    final storage = FirebaseStorage.instance;
+
+    // Sets the storage path to a folder of the user's UID inside the
+    // userMedia folder, followed by the file type detected by Mime.
+    // For example, if I uploaded a png, it would be
+    // userMedia/123456789/image-123456789.png
+    Reference ref = storage.ref(
+        'userMedia/${FirebaseAuth.instance.currentUser
+            ?.uid}/$name-${const Uuid().v4()}$fileExtension');
+
+    // Creates a metadata object from the raw bytes of the image,
+    // and then sets it to the reference above
+    SettableMetadata metadata =
+    SettableMetadata(contentType: lookupMimeType('', headerBytes: file));
+
+    await ref.putData(file, metadata);
+
+    // Gets the URL of this newly created object
+    var downloadUrl = ref.getDownloadURL();
+
+    return downloadUrl;
+  } else {
+    return null;
+  }
 }
